c1dfc59bf5e685e959fad2d1f0f6026fa0fb347c
habituatedcoder <work.mahmudulhasan@gmail.com>
2022-04-05 21:14:33 +0600

    data added

--- src/hooks/Dashboard/Dashboard.js
+++ src/hooks/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MyChart from '../../components/mychart/MyChart';
 
 const Dashboard = () => {
     return (
         <div>
             <h1>this is from dashboard </h1>
             <h2>huh ha ha ha ha</h2>
+            <MyChart></MyChart>
         </div>
     );
 };
